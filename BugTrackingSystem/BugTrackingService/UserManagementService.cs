c7ea2987191ff64540ebf846c5b2f72b927c19c3
Devang Bhalala <bhalaladevang@gmail.com>
2021-02-15 15:47:54 +0530

    Implemented Service Interface of User management

--- BugTrackingSystem/BugTrackingService/UserManagementService.cs
+++ BugTrackingSystem/BugTrackingService/UserManagementService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -10,8 +11,34 @@ namespace BugTrackingService
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserManagementService" in both code and config file together.
     public class UserManagementService : IUserManagementService
     {
-        public void DoWork()
+        string IUserManagementService.AddUserRecord(Person _person)
         {
+            throw new NotImplementedException();
+        }
+
+        string IUserManagementService.DeleteUserRecord(int _personId, UserRole _role)
+        {
+            throw new NotImplementedException();
+        }
+
+        DataSet IUserManagementService.GetAllUserRecordsByRole(UserRole _role)
+        {
+            throw new NotImplementedException();
+        }
+
+        DataSet IUserManagementService.GetUserRecord(int _id, UserRole _role)
+        {
+            throw new NotImplementedException();
+        }
+
+        DataSet IUserManagementService.GetUserRecordByPersonId(int _personId, UserRole _role)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IUserManagementService.UpdateUserRecord(Person _person)
+        {
+            throw new NotImplementedException();
         }
     }
 }
