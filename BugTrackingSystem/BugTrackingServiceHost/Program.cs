10f41b434717ab6432915dd19328d8783608abaf
Devang Bhalala <bhalaladevang@gmail.com>
2021-02-15 21:40:07 +0530

    Hosted User Management Service on console in program.cs

--- BugTrackingSystem/BugTrackingServiceHost/Program.cs
+++ BugTrackingSystem/BugTrackingServiceHost/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BugTrackingService;
+using System.ServiceModel;
 
 namespace BugTrackingServiceHost
 {
@@ -10,6 +12,16 @@ namespace BugTrackingServiceHost
     {
         static void Main(string[] args)
         {
+            Type userManageSerType = typeof(UserManagementService);
+            Uri userManageSerTcpUri = new Uri("net.tcp://localhost:8010/UserManagementService");
+            Uri userManageSerHttpUri = new Uri("http://localhost:8000/UserManagementService");
+            ServiceHost userServiceHost = new ServiceHost(userManageSerType, userManageSerTcpUri, userManageSerHttpUri);
+            userServiceHost.Open();
+            Console.WriteLine("User Management Service Hosted Successfully...........\n\n");
+            Console.ReadLine();
+            Console.ReadLine();
+            Console.ReadLine();
+            userServiceHost.Close();
         }
     }
 }
